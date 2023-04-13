94c30d76fca4341e08e262a5cfa4c94995f0e469
Vincent Chevalier <chev0100@algonquinlive.com>
2023-04-12 22:23:44 -0400

    add todos

--- src/router/auth.js
+++ src/router/auth.js
@@ -7,6 +7,8 @@ require("../utils/passport");
 
 const authRouter = Router();
 
+//TODO: refactor this so it dynamically redirects to google with a redirect url in the request.query
+// see line 11 to 21 in week14 /router/auth.js for the code
 // this redirects to google
 authRouter.get(
   "/google",
@@ -14,6 +16,8 @@ authRouter.get(
 );
 
 // google sends response to this
+//TODO: refactor this so that it uses the JWT and redirects with the token in the query string of our url in our React app
+// see line 24 to 42 in week14 /router/auth.js for the code
 authRouter.get(
   "/google/callback",
   passport.authenticate("google", { failureRedirect: "/" }),
