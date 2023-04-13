880ded3d02ea823a84f5c6fb4b211e4e0fe71892
Taylor Ramirez <rami0101@algonquinlive.com>
2023-04-13 16:26:49 -0400

    Added isAuth validation.

--- src/middleware/isAuthenticated.js
+++ src/middleware/isAuthenticated.js
@@ -1,12 +1,39 @@
-const isAuthenticated = (req, res, next) => {
-    if (!req.user) {
-      res.status(401).json({
-        error: "You must be signed in to see this.",
-      });
-      return;
+"use strict";
+
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+const { UnauthorizedError, NotFoundError } = require("../utils/errors");
+
+const isAuthenticated = async (req, res, next) => {
+  // look up token in headers
+  const rawToken = req.headers.authorization;
+  const token = rawToken?.replace("Bearer ", "");
+
+  if (!token) {
+    return next(new UnauthorizedError("JWT not found"));
+  }
+
+  // verify token
+  try {
+    const { id } = jwt.verify(token, process.env.JWT_SECRET);
+    // look up a user from the id
+    const user = await User.findById(id);
+
+    // throw an error if user not found
+    if (!user) {
+      throw new NotFoundError("User not found");
     }
+
+    req.user = user;
+
     return next();
-  };
-  
-  module.exports = isAuthenticated;
-  
+
+    // attach the user to the request
+  } catch (error) {
+    // throw an error if not verified
+    next(new UnauthorizedError(error.message));
+  }
+};
+
+module.exports = isAuthenticated;
