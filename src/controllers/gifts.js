470830807c20300f034a79ddbc1fa5020cdb4db9
Vincent Chevalier <chev0100@algonquinlive.com>
2023-04-14 13:22:17 -0400

    added getAll, getOne, replace, deleteOne

---
+++ src/controllers/gifts.js
@@ -0,0 +1,89 @@
+'use strict';
+
+const GiftService = require('../services/gifts');
+
+// GET (all)
+const getAll = async (req, res, next) => {
+	try {
+		// people/:id
+		const { id: personId } = req.params;
+		const gifts = await GiftService.getAll(personId);
+		res.json({ data: gifts });
+	} catch (error) {
+		next(error);
+	}
+};
+// GET
+const getOne = async (req, res, next) => {
+	// people/:id/gifts/:giftId
+	try {
+		//TODO: we need to make sure that the gifts belong to the person who owns them
+		// look at create service and controller
+		// const { id: personId } = req.params;
+		// const gifts = await GiftService.getOne(req.params.giftId, personId);
+		const gifts = await GiftService.getOne(req.params.giftId);
+		res.json({ data: gifts });
+	} catch (error) {
+		next(error);
+	}
+};
+
+// POST
+const create = async (req, res, next) => {
+	const { id: personId } = req.params;
+
+	try {
+		const createdGift = await GiftService.create(personId, req.sanitizedBody);
+
+		res.json({ data: createdGift });
+	} catch (error) {
+		next(error);
+	}
+};
+
+// PATCH
+const update = async (req, res, next) => {
+	const { id: personId, giftId } = req.params;
+
+	try {
+		const updatedGift = await GiftService.update(
+			personId,
+			giftId,
+			req.sanitizedBody
+		);
+
+		res.json({ data: updatedGift });
+	} catch (error) {
+		next(error);
+	}
+};
+
+// PUT
+const replace = async (req, res, next) => {
+	const { id: personId, giftId } = req.params;
+	try {
+		const replacedGift = await GiftService.replace(
+			personId,
+			giftId,
+			req.sanitizedBody
+		);
+		res.json({ data: replacedGift });
+	} catch (error) {
+		next(error);
+	}
+};
+
+// DELETE (one)
+const deleteOne = async (req, res, next) => {
+	try {
+		const deletedgift = await GiftService.deleteOne(req.params.giftId);
+		res.json({ data: deletedgift });
+	} catch (error) {
+		next(error);
+	}
+};
+
+module.exports = {
+	create,
+	update,
+};
