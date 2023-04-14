6c566e14792023b57427bf81fb783355d4c80cd8
Taylor Ramirez <rami0101@algonquinlive.com>
2023-04-14 11:51:40 -0400

    no message

--- src/middleware/isAuthenticated.js
+++ src/middleware/isAuthenticated.js
@@ -7,6 +7,7 @@ const { UnauthorizedError, NotFoundError } = require("../utils/errors");
 
 const isAuthenticated = async (req, res, next) => {
   // look up token in headers
+  // const rawToken = req.headers.authorization;
   const rawToken = req.query.token;
 
   const token = rawToken?.replace("Bearer ", "");
