50072002501ead17672d68f5a0216d13c21fb4ec
Vincent Chevalier <chev0100@algonquinlive.com>
2023-04-15 16:10:29 -0400

    added condition to rawtoken variable

--- src/middleware/isAuthenticated.js
+++ src/middleware/isAuthenticated.js
@@ -1,41 +1,40 @@
-"use strict";
+'use strict';
 
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
-const User = require("../models/user");
-const { UnauthorizedError, NotFoundError } = require("../utils/errors");
+const User = require('../models/user');
+const { UnauthorizedError, NotFoundError } = require('../utils/errors');
 
 const isAuthenticated = async (req, res, next) => {
-  // look up token in headers
-  // const rawToken = req.headers.authorization;
-  const rawToken = req.query.token;
+	// look up token in headers
+	const rawToken = req.query.token ?? req.headers.authorization;
 
-  const token = rawToken?.replace("Bearer ", "");
+	const token = rawToken?.replace('Bearer ', '');
 
-  if (!token) {
-    return next(new UnauthorizedError("JWT not found"));
-  }
+	if (!token) {
+		return next(new UnauthorizedError('JWT not found'));
+	}
 
-  // verify token
-  try {
-    const { id } = jwt.verify(token, process.env.JWT_SECRET);
-    // look up a user from the id
-    const user = await User.findById(id);
+	// verify token
+	try {
+		const { id } = jwt.verify(token, process.env.JWT_SECRET);
+		// look up a user from the id
+		const user = await User.findById(id);
 
-    // throw an error if user not found
-    if (!user) {
-      throw new NotFoundError("User not found");
-    }
+		// throw an error if user not found
+		if (!user) {
+			throw new NotFoundError('User not found');
+		}
 
-    req.user = user;
+		req.user = user;
 
-    return next();
+		return next();
 
-    // attach the user to the request
-  } catch (error) {
-    // throw an error if not verified
-    next(new UnauthorizedError(error.message));
-  }
+		// attach the user to the request
+	} catch (error) {
+		// throw an error if not verified
+		next(new UnauthorizedError(error.message));
+	}
 };
 
 module.exports = isAuthenticated;
