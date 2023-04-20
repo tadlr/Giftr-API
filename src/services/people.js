4a787a974b65c6112f078cbfb070fe9e7cdb7972
Vincent Chevalier <chev0100@algonquinlive.com>
2023-04-20 15:28:37 -0400

    UnauthorizedError validation for replace route

--- src/services/people.js
+++ src/services/people.js
@@ -1,84 +1,92 @@
-"use strict";
+'use strict';
 
-const Person = require("../models/person");
+const Person = require('../models/person');
 const {
-  BadRequestError,
-  NotFoundError,
-  UnauthorizedError,
-} = require("../utils/errors");
+	BadRequestError,
+	NotFoundError,
+	UnauthorizedError,
+} = require('../utils/errors');
 
 const getAll = async (ownerId) => {
-  const people = await Person.find({ ownerId }).select("name dob gifts");
-  return people;
+	const people = await Person.find({ ownerId }).select('name dob gifts');
+	return people;
 };
 
 const getOne = async (id, ownerId) => {
-  const foundPerson = await Person.findById(id);
-  if (foundPerson.ownerId.toString() != ownerId.toString()) {
-    throw new UnauthorizedError("This is not yours!!");
-  }
+	const foundPerson = await Person.findById(id);
+	if (foundPerson.ownerId.toString() != ownerId.toString()) {
+		throw new UnauthorizedError(
+			'You are not authorized to access this person.'
+		);
+	}
 
-  if (!foundPerson) throw new NotFoundError(`Person with id ${id} not found`);
-  return foundPerson;
+	if (!foundPerson) throw new NotFoundError(`Person with id ${id} not found`);
+	return foundPerson;
 };
 
 const create = async (personData) => {
-  const newPerson = new Person(personData);
-  await newPerson.save();
-  return newPerson;
+	const newPerson = new Person(personData);
+	await newPerson.save();
+	return newPerson;
 };
 
-const replace = async (id, personData) => {
-  if (!personData.name || !personData.dob)
-    throw new BadRequestError("Name and Date Of Birth (dob) are required");
+const replace = async (id, ownerId, personData) => {
+	if (!personData.name || !personData.dob)
+		throw new BadRequestError('Name and Date Of Birth (dob) are required');
 
-  const replacedPerson = await Person.findByIdAndUpdate(
-    id,
-    {
-      ...personData,
-    },
-    {
-      returnOriginal: false,
-    }
-  );
+	const replacedPerson = await Person.findByIdAndUpdate(
+		id,
+		{
+			...personData,
+		},
+		{
+			returnOriginal: false,
+		}
+	);
 
-  if (!replacedPerson)
-    throw new NotFoundError(`Person with id ${id} not found`);
+	if (replacedPerson.ownerId.toString() != ownerId.toString()) {
+		throw new UnauthorizedError(
+			'You are not authorized to access this person.'
+		);
+	}
 
-  return replacedPerson;
+	if (!replacedPerson)
+		throw new NotFoundError(`Person with id ${id} not found`);
+
+	return replacedPerson;
 };
 
 const update = async (id, updatedFields) => {
-  if (!Object.keys(updatedFields).length)
-    throw new BadRequestError("Nothing to update");
-  const updatedPerson = await Person.findByIdAndUpdate(
-    id,
-    {
-      ...updatedFields,
-    },
-    {
-      returnOriginal: false,
-    }
-  );
+	if (!Object.keys(updatedFields).length)
+		throw new BadRequestError('Nothing to update');
+	const updatedPerson = await Person.findByIdAndUpdate(
+		id,
+		{
+			...updatedFields,
+		},
+		{
+			returnOriginal: false,
+		}
+	);
 
-  if (!updatedPerson) throw new NotFoundError(`Person with id ${id} not found`);
+	if (!updatedPerson) throw new NotFoundError(`Person with id ${id} not found`);
 
-  return updatedPerson;
+	return updatedPerson;
 };
 
 const deleteOne = async (id) => {
-  const deletedPerson = await Person.findByIdAndDelete(id);
+	const deletedPerson = await Person.findByIdAndDelete(id);
 
-  if (!deletedPerson) throw new NotFoundError(`Person with id ${id} not found`);
+	if (!deletedPerson) throw new NotFoundError(`Person with id ${id} not found`);
 
-  return deletedPerson;
+	return deletedPerson;
 };
 
 module.exports = {
-  getAll,
-  getOne,
-  create,
-  replace,
-  update,
-  deleteOne,
+	getAll,
+	getOne,
+	create,
+	replace,
+	update,
+	deleteOne,
 };
